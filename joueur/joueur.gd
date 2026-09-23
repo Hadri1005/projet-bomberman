ad409bb257935eafa195a9e16f520b3086137261
smolzy <93006381+smolzy@users.noreply.github.com>
2026-09-23 08:56:22 +0200

    ajout collision joueur, fonction perte de hp et mort + retour au spawn quand dmg

--- joueur/joueur.gd
+++ joueur/joueur.gd
@@ -2,8 +2,22 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
-
-
+@export var max_health: int = 3
+var current_health: int
+@export var spawn_point: Vector3 = Vector3(0, 0, 0)
+func _ready() -> void:
+	current_health = max_health
+	
+func take_damage() -> void:
+	current_health = clampi(current_health - 1, 0, max_health)
+	global_position = spawn_point 
+	print("Current Health: ", current_health)
+	if current_health <= 0:
+		die()
+		
+func die() -> void:
+	print("Player died!")
+	
 func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
@@ -18,3 +32,5 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+func _on_button_pressed() -> void:
+	take_damage() # Replace with function body.
